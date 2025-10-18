7df6d614242a6ace9dd2b2fdb074dbd42b6bfeae
WellHelga <olga.usanova.06@mail.ru>
2025-10-19 00:06:20 +0700

    Delete accum.py

--- accum.py
+++
@@ -1,9 +0,0 @@
-def accum(x):
-    y1 = []
-    for i, j in enumerate(x):
-        y2 = j.upper() + j.lower() * i
-        y1.append(y2)
-    return '-'.join(y1)
-z = input()
-res = accum(z)
-print(res)
